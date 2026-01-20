e3488422474e7027da5cbc2e7b5849e309785e38
vishalsingh1616 <vishal1357singh@gmail.com>
2026-01-20 13:05:15 +0530

    Fix database config for Render (SQLite)

--- storefront/settings/production.py
+++ storefront/settings/production.py
@@ -5,5 +5,16 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['.onrender.com']
 
-ALLOWED_HOSTS = []
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
